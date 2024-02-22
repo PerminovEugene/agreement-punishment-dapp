887ded72dec95b925a38653ffd8756071fa39710
Evgenii Perminov <perminov.eugene@gmail.com>
2024-02-22 23:03:03 +0200

    Update deploy script a bit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,13 @@ import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      gas: 3000,
+      gasPrice: 1000,
+      blockGasLimit: 100000000000
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,39 @@
-import { ethers } from "hardhat";
+import { Wallet, NonceManager, BaseContract } from "ethers";
+import { ethers, artifacts } from "hardhat";
+import { RogueToken } from '../typechain-types';
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("RogueToken", [unlockTime], {
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+async function createContract(contractName: string, wallet: Wallet) {
+  const rogueToken = await artifacts.readArtifact(contractName);
+  const abi = rogueToken.abi;
+  const bytecode = rogueToken.bytecode;
+  return new ethers.ContractFactory(abi, bytecode, wallet);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function main() {
+  const provider = new ethers.JsonRpcProvider("http://localhost:8545");
+
+  const privateKey = "0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e"; // TODO use only for local dev
+  const wallet = new ethers.Wallet(privateKey, provider);
+
+  await wallet.getNonce();
+  const rogueTokenFactory = await createContract("RogueToken", wallet);
+  const rogueToken = await rogueTokenFactory.deploy() as RogueToken;
+  await rogueToken.waitForDeployment();
+  console.log(`Contract RogueToken deployed`);
+
+
+  await wallet.getNonce();
+  const agreementManagerFactory = await createContract("AgreementManager", wallet);
+  const agreementManager = await agreementManagerFactory.deploy();
+  await agreementManager.waitForDeployment();
+
+  const agreementManagerContractAddress = await agreementManager.getAddress();
+  console.log(`Contract AgreementManager deployed to: ${agreementManagerContractAddress}`);
+
+  await wallet.getNonce();
+  await rogueToken.setAuthorizedContract(agreementManagerContractAddress);
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
